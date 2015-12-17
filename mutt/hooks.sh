21eaeb80ffb483c0528c0cbdfb624c9a9d67c402
Tim Reddehase <robustus@rightsrestricted.com>
2015-12-17 22:30:28 +0100

    mutt: fix hook-generation for offlineimap.

--- mutt/hooks.sh
+++ mutt/hooks.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-base_directory="/Users/robustus/.Mail/"
+default_mail_base_dir=~/.Mail
+default_absolute_path=1
 
-directory=$1
-replacement=$2
+if (( $# < 2 )); then
+  >&2 echo "You need to provide a directory in mail base dir and the offlineimap name."
+  >&2 echo "Optionally set MAIL_BASE_DIR= if <${default_mail_base_dir}> doesn't suit you."
+  >&2 echo "You can set ABSOLUTE_PATH= to another value if <${default_absolute_path}> doesn't suit you."
+  exit 1
+fi
 
-# for folder in $(find "$base_directory$directory" -type d | grep -vP "(cur|new|tmp)$" | sed "s/^.+$directory//"); do
-#   echo $folder
-# done
+if [[ -z "${MAIL_BASE_DIR}" ]]; then
+  MAIL_BASE_DIR="${default_mail_base_dir}"
+fi
 
-for folder in $(ls -p "$base_directory/$directory" | grep -oP ".+(?=/)"); do
-  echo "folder-hook \"+\/$folder\" 'macro index o \"<shell-escape>offlineimap -qf $folder -a $replacement<enter>\"'"
+if [[ -z "${ABSOLUTE_PATH}" ]]; then
+  ABSOLUTE_PATH=${default_absolute_path}
+fi
 
+directory="$1"
+replacement="$2"
+
+folder-hook-target() {
+  folder="$1"
+  if (( $ABSOLUTE_PATH > 0 )); then
+    echo "${MAIL_BASE_DIR}/${directory}/${folder}"
+  else
+    echo "+\/$folder"
+  fi
+}
+# for folder in $(ls -p "${MAIL_BASE_DIR}/${directory}" | grep -oP ".+(?=/)"); do
+for folder in $(ls -1p ${MAIL_BASE_DIR}/${directory}/ | ggrep -oP ".+(?=/)"); do
+  full_path="$(folder-hook-target $folder)"
+  target="$(echo ${full_path} | sed 's/\//\\\//g')"
+  echo "folder-hook \"${target}\" 'macro index o \"<shell-escape>offlineimap -qf ${folder} -a ${replacement}<enter>\"'"
 done
